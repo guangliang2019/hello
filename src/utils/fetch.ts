7fc4d9d3df04f25aaa88f6f077829638180ba9a0
gl2020 <2606298246@qq.com>
2022-07-01 20:46:53 +0800

    fetch

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -6,23 +6,14 @@ import axios from "axios";
  * @returns 机器人回答的文本
  */
 export const fetchQuestion = (text: string): any => {
-  const url = `47.105.187.18:5005/robot?text=${text}`
+  const url = `http://47.105.187.18:5005/robot?text=${text}`
 
   //fetch(`47.105.187.18:5005/robot?text=${text}`).then(res => console.log(res))
   //axios.get(`47.105.187.18:5005/robot?text=${text}`).then(res => console.log(res))
 
-  const httpRequest = new XMLHttpRequest();
-  httpRequest.onreadystatechange = function () {
-    console.log(httpRequest.readyState);
-    if (httpRequest.readyState === 4) {
-      if (httpRequest.status === 200) {
-        console.log(httpRequest.response);
-        const data = JSON.parse(httpRequest.response);
-        console.log(data);
-      }
-    }
-  }
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url, false);
+  xhr.send(null);  //发送请求
+  console.log(xhr.responseText);  //接收数据
 
-  httpRequest.open('GET', url, true);
-  //httpRequest.setRequestHeader("Content-Type", "application/json");
 }
